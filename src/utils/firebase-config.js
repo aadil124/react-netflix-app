ca0ca625b620eed6e35ee603db6c0b30f5010063
aadil124 <ansariaadil124@gmail.com>
2023-08-09 20:08:11 +0530

    add firebase for authentication

---
+++ src/utils/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase-auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXqSLbqImEn23J-0hwQZKCVa6eM5Fuhfo",
+  authDomain: "netflix-app-d6b42.firebaseapp.com",
+  projectId: "netflix-app-d6b42",
+  storageBucket: "netflix-app-d6b42.appspot.com",
+  messagingSenderId: "130367664465",
+  appId: "1:130367664465:web:e7ad7096442807dfe9e370",
+  measurementId: "G-XSGSRE4QWC",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
