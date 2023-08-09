2a1877768950df86875cbab4b7bb2d38147cb9e2
aadil124 <ansariaadil124@gmail.com>
2023-08-09 20:07:19 +0530

    add router to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Netflix from "./pages/Netflix";
+import Signup from "./pages/Signup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
