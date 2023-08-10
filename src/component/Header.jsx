f691b403255379d1ab0550f620dcba4cf19de6d3
aadil124 <ansariaadil124@gmail.com>
2023-08-10 22:07:08 +0530

    update header component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,20 +3,40 @@ import logo from "../assets/logo.png";
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-const Container = styled.div``;
 
 const Header = (props) => {
   const navigate = useNavigate();
   return (
-    <Container>
+    <Container className="flex a-center j-between">
       <div className="logo">
         <img src={logo} alt="Netflix Logo" />
       </div>
-      <button onClick={navigate(props.login ? "/login" : "/signup")}>
+      <button
+        className="button"
+        onClick={navigate(props.login ? "/login" : "/signup")}
+      >
         {props.login ? "Login" : "Signup"}
       </button>
     </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 0 4rem;
+  .logo {
+    img {
+      height: 5rem;
+    }
+  }
+  .button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
+  }
+`;
 
 export default Header;
