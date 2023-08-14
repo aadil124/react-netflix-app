e15e692a92ea528290db6f5c4fa1770561668e02
aadil124 <ansariaadil124@gmail.com>
2023-08-14 23:09:17 +0530

    update firebase auth

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase-auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXqSLbqImEn23J-0hwQZKCVa6eM5Fuhfo",
