b7e3f8978151ec3c94f80be0c7dc1078f80e6564
aadil124 <ansariaadil124@gmail.com>
2023-09-12 19:47:23 +0530

    updated bg image

---
+++ src/component/bg/BackgroundImage.jsx
@@ -0,0 +1,13 @@
+import background from "../../assets/Login.jpg";
+import { Container } from "./styles";
+
+// background image for the login signup
+const BackgroundImage = () => {
+  return (
+    <Container>
+      <img src={background} alt="bg image" />
+    </Container>
+  );
+};
+
+export default BackgroundImage;
